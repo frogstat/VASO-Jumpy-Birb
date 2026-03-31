b295bbd94dd228be30c011688a3069affcea33e8
frogstat <frogstatftw@gmail.com>
2026-03-31 15:55:23 +0200

    Add game jar to .gitignore

--- build-jar.bat
+++ build-jar.bat
@@ -1,5 +1,5 @@
 @echo off
 call .\gradlew.bat lwjgl3:jar
-copy /Y ".\lwjgl3\build\libs\Spel-1.0.0.jar" ".\Spel-1.0.0.jar"
+copy /Y ".\lwjgl3\build\libs\Spel-1.0.0.jar" ".\donup.jar"
 echo Klar! Jar-filen finns nu i projektmappen.
 pause
