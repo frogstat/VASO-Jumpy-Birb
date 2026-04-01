5abb08b1008c9c63cf7f974cec6aea9713cdb94e
frogstat <frogstatftw@gmail.com>
2026-04-01 10:13:40 +0200

    Update build scripts to put jar file in separate folder

--- build-jar.bat
+++ build-jar.bat
@@ -1,5 +1,5 @@
 @echo off
 call .\gradlew.bat lwjgl3:jar
-copy /Y ".\lwjgl3\build\libs\Spel-1.0.0.jar" ".\donup.jar"
-echo Klar! Jar-filen finns nu i projektmappen.
+copy /Y ".\lwjgl3\build\libs\Spel-1.0.0.jar" ".\Final Jar\donup.jar"
+echo Klar! Jar-filen finns nu i ./Final Jar
 pause
--- build-jar.sh
+++ build-jar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./gradlew lwjgl3:jar
-cp ./lwjgl3/build/libs/Spel-1.0.0.jar ./donup.jar
-echo "Klar! Jar-filen finns nu i projektmappen."
+cp ./lwjgl3/build/libs/Spel-1.0.0.jar "./Final Jar/donup.jar"
+echo "Klar! Jar-filen finns nu i ./Final Jar"
