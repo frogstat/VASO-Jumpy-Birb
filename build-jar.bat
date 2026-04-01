f8aba050b0ddf0abaf930c0cf488c06c0f883657
frogstat <frogstatftw@gmail.com>
2026-04-01 11:56:53 +0200

    Edit scripts to create Final Jar folder if it doesn't exist

--- build-jar.bat
+++ build-jar.bat
@@ -1,5 +1,6 @@
 @echo off
 call .\gradlew.bat lwjgl3:jar
+if not exist ".\Final Jar" mkdir ".\Final Jar"
 copy /Y ".\lwjgl3\build\libs\Spel-1.0.0.jar" ".\Final Jar\donup.jar"
 echo Klar! Jar-filen finns nu i ./Final Jar
 pause
--- build-jar.sh
+++ build-jar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./gradlew lwjgl3:jar
+mkdir -p "./Final Jar"
 cp ./lwjgl3/build/libs/Spel-1.0.0.jar "./Final Jar/donup.jar"
 echo "Klar! Jar-filen finns nu i ./Final Jar"
