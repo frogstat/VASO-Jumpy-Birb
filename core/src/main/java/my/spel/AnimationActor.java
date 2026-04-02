0d5807d004c05783bcb1dd0ddca24d3ee54a9593
Sanna Ranta <sranta39@gmail.com>
2026-04-02 05:50:18 +0200

    add menu title highlight animation

--- core/src/main/java/my/spel/AnimationActor.java
+++ core/src/main/java/my/spel/AnimationActor.java
@@ -1,4 +1,52 @@
 package my.spel;
 
-public class AnimationActor {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class AnimationActor extends Actor {
+    private final Animation<Texture> animation;
+    private float elapsedTime;
+    private final float pauseTimeSeconds;
+    private float timeWhenAnimationPaused;
+    private boolean animationPaused;
+    private final float x;
+    private final float y;
+
+    public AnimationActor(Animation<Texture> animation, float pauseTimeSeconds, float x, float y) {
+        this.animation = animation;
+        this.pauseTimeSeconds = pauseTimeSeconds;
+        animationPaused = false;
+        timeWhenAnimationPaused = 0;
+        elapsedTime = 0;
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        elapsedTime += delta;
+
+        if (animationPaused && elapsedTime - timeWhenAnimationPaused > pauseTimeSeconds) {
+            elapsedTime = 0;
+            animationPaused = false;
+        }
+    }
+
+    // Draw at every frame
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+
+        // if animation not paused, show animation
+        if (!animationPaused) {
+            final Texture texture = animation.getKeyFrame(elapsedTime);
+            batch.draw(texture, x, y, texture.getWidth(), texture.getHeight());
+            if (animation.isAnimationFinished(elapsedTime)) {
+                timeWhenAnimationPaused = elapsedTime;
+                animationPaused = true;
+            }
+        }
+    }
 }
